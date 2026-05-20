43f8d4a602be9b81ffe5a3abc84cc5e39a61ba48
S. S. Mahmud Turza <mahmudturza@gmail.com>
2026-05-20 17:41:34 +0600

    updated vite configuration

--- react_frontend/vite.config.js
+++ react_frontend/vite.config.js
@@ -10,10 +10,10 @@ export default defineConfig({
     },
   },
   server: {
-    port: 3000,
+    // port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://iubat-qa-platform.onrender.com',
         changeOrigin: true,
       },
     },
